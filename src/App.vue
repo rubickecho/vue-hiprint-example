3a0db05b04fb3357a46902a6b741313ccab8f214
peakcool <tangliangcheng852@gmail.com>
2019-07-31 09:49:52 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="app">
+		<router-view />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'App'
+}
+</script>
+
+<style>
+* {
+	padding: 0;
+	margin: 0;
+}
+
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: left;
+	color: #2c3e50;
+	height: 100%;
+	width: 100%;
+}
+</style>
